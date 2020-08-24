0190b52b7131eb622097d6c314ecfa81e9e383eb
Smart Tool Factory <smarttoolfactory@icloud.com>
2020-08-24 10:16:41 +0300

    Move data source implementations to separate folder

--- libraries/data/src/main/java/com/smarttoolfactory/data/source/PostDataSource.kt
+++ libraries/data/src/main/java/com/smarttoolfactory/data/source/PostDataSource.kt
@@ -1,9 +1,5 @@
 package com.smarttoolfactory.data.source
 
-import com.smarttoolfactory.data.api.PostApi
-import com.smarttoolfactory.data.api.PostApiRxJava
-import com.smarttoolfactory.data.db.PostDao
-import com.smarttoolfactory.data.db.PostDaoRxJava3
 import com.smarttoolfactory.data.model.PostDTO
 import com.smarttoolfactory.data.model.PostEntity
 import io.reactivex.rxjava3.core.Completable
@@ -14,7 +10,6 @@ interface PostDataSource
 /*
     Coroutines
  */
-
 interface RemotePostDataSource : PostDataSource {
     suspend fun getPostDTOs(): List<PostDTO>
 }
@@ -23,30 +18,6 @@ interface LocalPostDataSource : PostDataSource {
     suspend fun getPostEntities(): List<PostEntity>
     suspend fun saveEntities(posts: List<PostEntity>): List<Long>
     suspend fun deletePostEntities()
-}
-
-class RemotePostDataSourceImpl(private val postApi: PostApi) : RemotePostDataSource {
-
-    override suspend fun getPostDTOs(): List<PostDTO> {
-        return postApi.getPosts()
-    }
-}
-
-class LocalPostDataSourceImpl(private val postDao: PostDao) :
-    LocalPostDataSource {
-
-    override suspend fun getPostEntities(): List<PostEntity> {
-        return postDao.getPostList()
-    }
-
-    override suspend fun saveEntities(posts: List<PostEntity>): List<Long> {
-
-        return postDao.insert(posts)
-    }
-
-    override suspend fun deletePostEntities() {
-        postDao.deleteAll()
-    }
 }
 
 /*
@@ -61,28 +32,4 @@ interface LocalPostDataSourceRxJava3 : PostDataSource {
     fun getPostEntities(): Single<List<PostEntity>>
     fun saveEntities(posts: List<PostEntity>): Completable
     fun deletePostEntities(): Completable
-}
-
-class RemoteDataSourceRxJava3Impl(private val postApi: PostApiRxJava) :
-    RemotePostDataSourceRxJava3 {
-
-    override fun getPostDTOs(): Single<List<PostDTO>> {
-        return postApi.getPostsSingle()
-    }
-}
-
-class LocalDataSourceRxJava3Impl(private val postDaoRxJava3: PostDaoRxJava3) :
-    LocalPostDataSourceRxJava3 {
-
-    override fun getPostEntities(): Single<List<PostEntity>> {
-        return postDaoRxJava3.getPostsSingle()
-    }
-
-    override fun saveEntities(posts: List<PostEntity>): Completable {
-        return postDaoRxJava3.insert(posts)
-    }
-
-    override fun deletePostEntities(): Completable {
-        return postDaoRxJava3.deleteAll()
-    }
 }
---
+++ libraries/data/src/main/java/com/smarttoolfactory/data/source/PostDataSourceImpl.kt
@@ -0,0 +1,66 @@
+package com.smarttoolfactory.data.source
+
+import com.smarttoolfactory.data.api.PostApi
+import com.smarttoolfactory.data.api.PostApiRxJava
+import com.smarttoolfactory.data.db.PostDao
+import com.smarttoolfactory.data.db.PostDaoRxJava3
+import com.smarttoolfactory.data.model.PostDTO
+import com.smarttoolfactory.data.model.PostEntity
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
+
+/*
+    *** Coroutines Implementations for PostDataSources ***
+ */
+class RemotePostDataSourceImpl @Inject constructor(private val postApi: PostApi) :
+    RemotePostDataSource {
+
+    override suspend fun getPostDTOs(): List<PostDTO> {
+        return postApi.getPosts()
+    }
+}
+
+class LocalPostDataSourceImpl @Inject constructor(private val postDao: PostDao) :
+    LocalPostDataSource {
+
+    override suspend fun getPostEntities(): List<PostEntity> {
+        return postDao.getPostList()
+    }
+
+    override suspend fun saveEntities(posts: List<PostEntity>): List<Long> {
+
+        return postDao.insert(posts)
+    }
+
+    override suspend fun deletePostEntities() {
+        postDao.deleteAll()
+    }
+}
+
+/*
+    *** RxJava3 Implementations for PostDataSources ***
+ */
+class RemoteDataSourceRxJava3Impl @Inject constructor(private val postApi: PostApiRxJava) :
+    RemotePostDataSourceRxJava3 {
+
+    override fun getPostDTOs(): Single<List<PostDTO>> {
+        return postApi.getPostsSingle()
+    }
+}
+
+class LocalDataSourceRxJava3Impl @Inject constructor(private val postDaoRxJava3: PostDaoRxJava3) :
+    LocalPostDataSourceRxJava3 {
+
+    override fun getPostEntities(): Single<List<PostEntity>> {
+        return postDaoRxJava3.getPostsSingle()
+    }
+
+    override fun saveEntities(posts: List<PostEntity>): Completable {
+        return postDaoRxJava3.insert(posts)
+    }
+
+    override fun deletePostEntities(): Completable {
+        return postDaoRxJava3.deleteAll()
+    }
+}
